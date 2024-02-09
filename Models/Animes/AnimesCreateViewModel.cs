834159bd5699f64920e58a654dbb22d5a7d58fd4
Probošt Vojtěch <vojtaproba@seznam.cz>
2024-02-09 21:31:41 +0100

    Přidejte soubory projektu.

---
+++ Models/Animes/AnimesCreateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using static AnimeList_AspNetCore_EF.Core.Entities.Anime;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AnimeList_AspNetCore_EF.Models.Animes
+{
+    public class AnimesCreateViewModel
+    {
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Episodes { get; set; }
+        public string WatchedEpisodes { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+        public int Score { get; set; }
+        public Status WatchingStatus { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnimeList_AspNetCore_EF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
